6528aee735b9c6b4e532a3af1825431c3140e73d
Payal shirose <159715853+Payal109@users.noreply.github.com>
2025-06-16 20:22:37 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Fibonacci_series
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---Finbonacci series---");
+            int num = 8;
+            int a = 0;
+            int b = 1;
+            int c;
+            Console.Write(a + "   " + b);
+            while (num > 0)
+            {
+                c = a + b;
+                Console.Write("   " + c + "   ");
+                a = b; b = c;
+                num--;
+            }
+        }
+    }
+}
